24f3b29532177edac8d708435401b3a9662de373
Naveen Yadav <yn.naveen00@gmail.com>
2024-11-15 00:50:03 +0530

    Profile stated

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center align-middle'>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { GoHome } from 'react-icons/go'
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between border p-4'>
+            <div className='text-xl'>
+                Naveen Yadav.
+            </div>
+            <div className=''>
+                <ul className='flex '>
+                    <li className='px-4 text-xl flex justify-center align-middle'>
+                        <GoHome className='mt-1 mx-2' />Home
+                    </li>
+                    <li className='px-4 text-xl flex justify-center align-middle'>
+                        <GoHome className='mt-1 mx-2' />About
+                    </li>
+                    <li className='px-4 text-xl flex justify-center align-middle'>
+                        <GoHome className='mt-1 mx-2' />Skills
+                    </li>
+                    <li className='px-4 text-xl flex justify-center align-middle'>
+                        <GoHome className='mt-1 mx-2' />Projects
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
